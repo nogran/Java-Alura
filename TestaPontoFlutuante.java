b6e3a35adfce4cac7a879a60cce4b57f6c14d5b4
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-03-13 19:42:27 -0300

    difference between float and int

---
+++ TestaPontoFlutuante.java
@@ -0,0 +1,22 @@
+public class TestaPontoFlutuante {
+	
+	public static void main(String[] args) {
+		double salario;
+		salario = 1250.70;
+		System.out.println("meu salario e " + salario);
+		
+		double divisao;
+		divisao = 3.14 / 2;
+		System.out.println(divisao);
+		
+		int outraDivisao = 5 / 2;
+		System.out.println(outraDivisao);
+		
+		double novaTentativa = 5 / 2;
+		System.out.println(novaTentativa);
+		
+		double maisUmaTentativa = 5.0 / 2;
+		System.out.println(maisUmaTentativa);
+	}
+
+}
