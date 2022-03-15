0b459608857a10dffc08a4011492ad8d0531c6b4
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-03-15 15:49:58 -0300

    if boolean and double

---
+++ 06.Praticando_condicionais/TesteSalario.java
@@ -0,0 +1,16 @@
+public class TesteSalario {
+
+    public static void main(String[] args) {
+
+        boolean foiPromovido = true;
+		double salario;
+
+        if(foiPromovido) {
+            salario = 4200.0;
+        } else {
+            salario = 3800.0;
+        }
+
+        System.out.println(salario);
+    }
+}
